23b70b9d6a8d1d60296544cb9f8dca1231e8d70f
deepthiallamudi-ux <deepthiallamudi@gmail.com>
2025-11-22 22:14:48 +0530

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -7,3 +7,17 @@ const sqndcub=(n)=>({
     cube:n^3
 });
  console.log(sqndcub(7))
+
+//question 13//
+const people = [ { name: "Alice", 
+    address: { city: "New York", 
+        street: { name: "Broadway", 
+            number: 123 } } }, 
+    { name: "Bob", 
+        address: { city: "Los Angeles", 
+            street: { name: "Sunset Boulevard", 
+                number: 456 } } } ]
+
+const result = people.map(({ name, address: { city, street: { name: streetName } } }) => 
+    `${name} lives in ${city} on ${streetName}`);
+console.log(result);
