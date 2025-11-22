54bed5f0176693490a4d619d5de85e8db30fefb7
deepthiallamudi-ux <deepthiallamudi@gmail.com>
2025-11-23 01:27:20 +0530

    Create callback1.js

---
+++ callback1.js
@@ -0,0 +1,30 @@
+scores = [
+  { "name": "Alice", "marks": 58 },
+  { "name": "Bob", "marks": 85 },
+  { "name": "Charlie", "marks": 92 },
+  { "name": "David", "marks": 45 },
+  { "name": "Emma", "marks": 76 },
+  { "name": "Frank", "marks": 63 },
+  { "name": "Grace", "marks": 89 },
+  { "name": "Hannah", "marks": 95 },
+  { "name": "Ian", "marks": 54 },
+  { "name": "Jack", "marks": 79 },
+  { "name": "Kate", "marks": 67 },
+  { "name": "Leo", "marks": 88 },
+  { "name": "Mia", "marks": 91 },
+  { "name": "Nathan", "marks": 72 },
+  { "name": "Olivia", "marks": 82 }
+];
+
+function names(studentsArray) {
+
+   filterarr=studentsArray.filter(student => student.marks > 60);
+  
+  sorted = filterarr.sort((a, b) => b.marks - a.marks);
+  
+  names = sorted.map(student => student.name);
+  
+  console.log(names)
+}
+names(scores)
+
